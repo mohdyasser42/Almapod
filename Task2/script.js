3891f3780a33a4ab6cd4cf9fca5ac97ad6413937
mohdyasser42 <mohd535643@gmail.com>
2023-01-05 21:25:01 +0530

    Tasks completed

---
+++ Task2/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $("#nav-bar-resp").click(function(){
+      $(".nav-resp").slideToggle("slow");
+    });
+  });
---
+++ Task3/script.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+    $("#exit").click(function(){
+      $(".promo").hide();
+    });
+});
+
+$(document).ready(function(){
+  $("#nav-icon1").click(function(){
+    $(".nav-slide").show();
+    $("#nav-icon1").hide();
+    $("#nav-icon2").show();
+  });
+  $("#nav-icon2").click(function(){
+    $(".nav-slide").hide();
+    $("#nav-icon1").show();
+    $("#nav-icon2").hide();
+  });
+});
+
+
+const newdate = "1/1/2024";
+
+const hourEl = document.querySelector('.hour');
+const minuteEl = document.querySelector('.minute');
+const secondEl = document.querySelector('.seconds');
+
+function timeCountDown() {
+  const nowDate = new Date();
+  const newDate2 = new Date(newdate);
+  const totalSeconds = (newDate2 - nowDate) / 1000;
+
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
+
+  hourEl.textContent = formatTime(hours);
+  minuteEl.textContent = formatTime(minutes);
+  secondEl.textContent = formatTime(seconds);
+
+}
+
+function formatTime(time) {
+  return time > 10 ? time : `0${time}`;
+}
+
+timeCountDown()
+setInterval(timeCountDown, 1000);
